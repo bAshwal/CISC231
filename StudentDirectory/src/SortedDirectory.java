c74179832e60579e53533e9dae7458122766dc86
bAshwal <b.ashwal@hotmail.com>
2016-09-14 21:12:27 -0500

    Add files via upload

--- StudentDirectory/src/SortedDirectory.java
+++ StudentDirectory/src/SortedDirectory.java
@@ -1,19 +1,59 @@
 
 public class SortedDirectory implements SDinterface {
+	private String name;
+	private int num;
+	private Student studentList[];
+	private Student tempStudentList[];
 	
-	private int [] studentID;
-	private String [] name;
-
-	private SortedDirectory(){
+	protected SortedDirectory(){
+		num=0;
+		
+		
+	}
+	private class Student{
+		private int studentID;
+		private String name;
+		
+		private Student(String name, int studentID){
+			this.name = name;
+			this.studentID = studentID;
+		}
 		
 	}
 	
+
 	public void addStudent (String name, int studentID){
+		
+		num++; //Increment size of the directory by one, since we are adding
+		
+		tempStudentList = new Student[num]; // Create a temporary list to store orignal array
+		
+		if (num>1){ // copy the array only if the size of the list is greater than one
+			for(int i=0; i<studentList.length;i++){// loop to copy current list to the temporary one.	
+				tempStudentList[i] = studentList[i];
+			}
+		
+		}
+		studentList= new Student[num];// init. an array of objects with new size
+
+		num--; // Decrement size to add a student object
+		
+		for(int i=0; i<studentList.length;i++){
+			studentList[i] = tempStudentList[i];
+		}
+		studentList[num]= new Student(name,studentID);// adding a new student
+		
+		num++; // Increment to reflect current size
 		
 	}
 	
 	public String prettyStr(){
-		return name[];
+		String dir = new String();
+		
+		for (int i=0;i<studentList.length;i++){
+			dir = dir + "Student name: " + studentList[i].name + "Student ID: " + studentList[i].studentID + "\n";
+		}
+		return dir;
 		
 	}
 	public void removeStudent (int studentID){
@@ -21,7 +61,7 @@ public class SortedDirectory implements SDinterface {
 	}
 	
 	public String studentLookup (int studentID){
-		return name[0];
+		return null;
 		
 	}
 	
--- StudentDirectory/src/test.java
+++ StudentDirectory/src/test.java
@@ -4,14 +4,17 @@ public class test {
 
 	public static void main(String[] args) {
 		
-	UnsortedDirectory d1 = new UnsortedDirectory();
+	SortedDirectory d1 = new SortedDirectory();
 	
-	//d1.addStudent(new Student("Ahmed",1923123));
-	//d1.addStudent("YoYo", 12312314);
-	//d1.addStudent("YoYo", 12312534);
-	//d1.addStudent("YoYo", 12314234);
-	//d1.addStudent("YoYo", 12313234);
-	//d1.addStudent("YoYo", 12231234);
+	d1.addStudent("YoYo", 12312314);
+	d1.addStudent("YoJYo", 12312534);
+	d1.addStudent("YoLYo", 12314234);
+	d1.addStudent("YoKYo", 12313234);
+	d1.addStudent("YoYo", 12231234);
+	d1.addStudent("YoYo", 12231234);
+	d1.addStudent("YoYo", 12231234);
+	d1.addStudent("YoYo", 12231234);
+	d1.addStudent("YoYo", 12231234);
 	System.out.println(d1.prettyStr());
 	}
 
