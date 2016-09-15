215a4afc1086bc35aba3151f32c745bfc1997689
bAshwal <b.ashwal@hotmail.com>
2016-09-15 17:08:34 -0500

    Add files via upload

--- StudentDirectory/src/SortedDirectory.java
+++ StudentDirectory/src/SortedDirectory.java
@@ -1,120 +1,125 @@
 
+
 public class SortedDirectory implements SDinterface {
-	private String name;
-	private int num;
+
+	private int listSize;
 	private Student studentList[];
 	private Student tempStudentList[];
-	
+
 	protected SortedDirectory(){
-		num=0;
-		studentList = new Student[num];
-		
-		
-		
+		listSize=0;
+		studentList = new Student[listSize];
 	}
+
 	private class Student{
 		private int studentID;
 		private String name;
-		
+
 		private Student(String name, int studentID){
 			this.name = name;
 			this.studentID = studentID;
 		}	
-		
+
 	}
-	
 
 	public void addStudent (String name, int studentID){
-		
-		if (studentExist(studentList,studentID)){
-			
+
+		if (getStudentIndex(studentList,studentID)>=0){
+
 			System.out.println("Sorry, you cannot add " + name + ",because his/her Student ID [" + studentID + "]already exist.");
-			
+
 		}else{
-			
 
-		num++; //Increment size of the directory by one, since we are adding
-		
-		tempStudentList = new Student[num]; // Create a temporary list to store orignal array
-		
-		if (num>1){ // copy the array only if the size of the list is greater than one
-			
-		copyCurListToTemp(studentList,tempStudentList);
-		}
-		
-		studentList= new Student[num];// init. an array of objects with new size
 
-		num--; // Decrement size to add a student object
-		
-		copyTempListToCur(studentList,tempStudentList);
-		
-		studentList[num]= new Student(name,studentID);// adding a new student
-		
-		num++; // Increment to reflect current size of our list
+			listSize++; 
+
+			tempStudentList = new Student[listSize]; 
+
+			if (listSize>1){ 
+
+				copyCurListToTemp(studentList,tempStudentList);
+			}
+
+			studentList= new Student[listSize];
+			
+			listSize--; 
+
+			copyTempListToCur(studentList,tempStudentList);
+
+			studentList[listSize]= new Student(name,studentID);
+
+			listSize++; 
 		}
+		orderStudents(listSize);
+
 	}
-	
+
 	public String prettyStr(){
+
 		String dir = "Student Name : Student ID: \n";
-		
+
 		for (int i=0;i<studentList.length;i++){
 			dir = dir + studentList[i].name + "          "+ studentList[i].studentID + "\n";
 		}
 		return dir;
-		
+
 	}
+
 	public void removeStudent (int studentID){
-		
-		if (studentExist(studentList,studentID)){
-			num--;
+
+		if (getStudentIndex(studentList,studentID)>=0){
+			listSize--;
 			for (int i =0; i<studentList.length;i++){
-				if ((studentList[i].studentID == studentID) && num ==0)
+				if ((studentList[i].studentID == studentID) && listSize ==0)
 				{
-					studentList = new Student[num];
+					studentList = new Student[listSize];
 				}else if (studentList[i].studentID == studentID)
 				{
 					Student toRemove = studentList[i];
-					studentList[i] = studentList[num];
-					studentList[num] = toRemove;
+					studentList[i] = studentList[listSize];
+					studentList[listSize] = toRemove;
 
 				}
 			}
-			
-			if (num>0){ // copy the array only if the size of the list is greater than one	
-			copyCurListToTemp(studentList,tempStudentList);
+
+			if (listSize>0){ // copy the array only if the size of the list is greater than one	
+				copyCurListToTemp(studentList,tempStudentList);
 			}
-			studentList= new Student[num];// init. an array of objects with new size
-			
-			if (num>=0){
-			copyTempListToCur(studentList,tempStudentList);
+			studentList= new Student[listSize];// init. an array of objects with new size
+
+			if (listSize>=0){
+				copyTempListToCur(studentList,tempStudentList);
 			}
 		}else{
-			
-			System.out.println("Sorry, you cannot remove this student, because his/her Student ID [" + studentID + "]does not exist.");
+
+			System.out.println("Sorry, you cannot remove this student, because his/her Student ID #[" + studentID + "] does not exist.");
 
 		}
+		orderStudents(listSize);
 	}
-	
+
 	public String studentLookup (int studentID){
-		
-		String found= new String();
-		if (studentExist(studentList,studentID)){
-			found = "Student with ID #" + studentID + " exists!";
+
+		String output= new String();
+		int studentIndex = getStudentIndex(studentList,studentID);
+		if (studentIndex<0){
+			output = "The student you are looking for is not in the directory.";
+		}else{
+			output = "The student you are looking for is " + studentList[studentIndex].name + ", with Student ID # [" + studentList[studentIndex].studentID + "] .\n";
 		}
-		return found;
-		
+		return output;
 	}
-	
-	private boolean studentExist(Student studentList[], int num){
+
+	private int getStudentIndex(Student studentList[], int num){
 		for (int i=0; i<studentList.length;i++)
 		{
 			if (studentList[i].studentID == num)
 			{
-				return true;
+				return i;
 			}
-			
+
 		}
-		return false;
+		return -1;
 	}
 	private void copyCurListToTemp(Student studentList[], Student tempStudentList[]){
 		for(int i=0; i<studentList.length;i++){// loop to copy current list to the temporary one.	
@@ -124,9 +129,33 @@ public class SortedDirectory implements SDinterface {
 	private void copyTempListToCur(Student studentList[], Student tempStudentList[]){
 		for(int i=0; i<studentList.length;i++){
 			studentList[i] = tempStudentList[i];
-			}
-		
+		}
+
 	}
-	
-	
+	private void orderStudents(int size)
+	{
+		if (size==0){
+
+		}else{
+
+			for (int i=0; i < studentList.length; i++)
+			{
+				Student Temp1 = studentList[i];
+				Student Temp2 = studentList[size-1];
+
+				if(studentList[i].studentID < studentList[size-1].studentID){
+
+					studentList[i] = Temp2;
+					studentList[size-1] = Temp1;
+
+				}
+
+				orderStudents(size-1);
+
+			}
+
+		}
+
+	}
+
 }
--- StudentDirectory/src/test.java
+++ StudentDirectory/src/test.java
@@ -3,25 +3,43 @@
 public class test {
 
 	public static void main(String[] args) {
+
+		SortedDirectory d1 = new SortedDirectory();
 		
-	SortedDirectory d1 = new SortedDirectory();
-	
-	d1.addStudent("Hubbly", 1);
-	d1.addStudent("Dubbly", 2);
-	d1.addStudent("Doohly", 3);
-	d1.addStudent("Daahly", 4);
-	d1.removeStudent(4);
-	d1.removeStudent(3);
-	d1.removeStudent(2);
-	d1.removeStudent(1);
-	d1.removeStudent(4);
-	d1.addStudent("Daahly", 4);
-	System.out.println(d1.prettyStr());
+		d1.addStudent("abc", 0);
+		d1.addStudent("def", 11);
+		d1.addStudent("ghi", 2);
+		d1.addStudent("jkl", 13);
+		d1.addStudent("mno", 1);
+		d1.addStudent("pqr", 14);
+		d1.addStudent("stu",103);
+		d1.addStudent("vwx", 0);
+//		d1.addStudent("yza", 9);
+//		d1.addStudent("bcd", 10);
+//		d1.addStudent("efg", 11);
+//		d1.addStudent("hij", 12);
+//		d1.addStudent("klm", 13);
+//		d1.addStudent("nop", 14);
+//		d1.addStudent("qrs", 15);
+//		d1.addStudent("tuv", 16);
+//		d1.addStudent("wxy", 17);
+//		d1.addStudent("zab", 18);
+//		d1.addStudent("cde", 19);
+//		d1.addStudent("fgh", 20);
+//		d1.addStudent("ijk", 21);
+//		d1.addStudent("lmn", 22);
+//		d1.addStudent("opq", 23);
+//		d1.addStudent("rst", 24);
+//		d1.addStudent("uvw", 25);
+//		d1.addStudent("xyz", 26);
+//		d1.removeStudent(1);
+//		System.out.println(d1.studentLookup(1));
+//		System.out.println(d1.prettyStr());
+		
+		System.out.println(d1.prettyStr());
+		System.out.println(d1.studentLookup(11));
+		
 
-
-
-
-	
 	}
 
 }
